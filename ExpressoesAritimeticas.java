0981bee223fffed87bf1da0d39058ef3198e74e5
Jardison Rebouças <jardisonrs@live.com>
2022-01-22 19:40:44 -0300

    novos arquivos

---
+++ ExpressoesAritimeticas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cursojavabasico;
+/*
+    Classe explicativa sobre as expressões aritméticas
+    Versão 1.0
+*/
+/**
+ *
+ * @author jardi
+ */
+public class ExpressoesAritimeticas {
+    public static void main(String[] args){
+        //int resultado = 10 + 2*3 / 2;
+        //boolean resultado = 27 + 40 > 100 - 50;
+        //resultado = 30 == 3 * 10 && 50 < 15;
+        double resultado = Math.PI;
+        //biblioteca math do java
+        System.out.println(resultado);
+    }
+}
